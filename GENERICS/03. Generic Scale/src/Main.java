a184c6d5e40bf887c8bba156f1d0acb08a945af2
Erik <vfxbaliof@gmail.com>
2019-02-09 15:01:31 +0200

    03. Generic Scale

---
+++ GENERICS/03. Generic Scale/src/Main.java
@@ -0,0 +1,17 @@
+import scale.Scale;
+
+public class Main {
+    public static void main(String[] args) {
+        Scale<Integer> integerScale = new Scale<>(10, 20);
+
+        System.out.println(integerScale.getHeavier());
+
+        Scale<Integer> integerScaleEqual = new Scale<>(10, 10);
+
+        System.out.println(integerScaleEqual.getHeavier());
+
+        Scale<String> stringScale = new Scale<>("A", "B");
+
+        System.out.println(stringScale.getHeavier());
+    }
+}
