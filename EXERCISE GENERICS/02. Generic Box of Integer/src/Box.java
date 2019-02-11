7fa3ac66d947e4bc99078758f37244cc7e4f94f4
Erik <vfxbaliof@gmail.com>
2019-02-11 16:57:21 +0200

    02. Generic Box of Integer

---
+++ EXERCISE GENERICS/02. Generic Box of Integer/src/Box.java
@@ -0,0 +1,12 @@
+public class Box<T> {
+    private T value;
+
+    public Box(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %s", value.getClass().getCanonicalName(), value);
+    }
+}
